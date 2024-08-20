9470418f5c7be3f55c68be5ddca748c7f1f116b8
MassMb <103121590+MassMb@users.noreply.github.com>
2024-08-20 09:07:12 +0200

    css commmit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { cn } from "@/lib/utils"
 import { Inter as FontSans } from "next/font/google"
+import Navbar from "@/components/ui/Navbar";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -25,7 +26,11 @@ export default function RootLayout({
         fontSans.variable
       )}
       >
-        {children}</body>
+        <Navbar />
+        <main>
+           {children} 
+        </main>
+        </body>
     </html>
   );
 }
